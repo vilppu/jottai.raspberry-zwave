2c6250836c9e2cad4abbba69b91cf32f3c6c9e53
Timo Vilppu <tsvilp@gmail.com>
2020-12-06 20:08:20 +0200

    Debug logging.

--- mapper.cpp
+++ mapper.cpp
@@ -3,5 +3,19 @@
 
 SensorData ToSensorData(OpenZWave::Notification const& notification)
 {
+	std::cout<<"GetValueID().GetAsString(): "<<notification.GetValueID().GetAsString()<<std::endl;
+	std::cout<<"GetValueID().GetTypeAsString(): "<<notification.GetValueID().GetTypeAsString()<<std::endl;
+	std::cout<<"GetValueID().GetGenreAsString(): "<<notification.GetValueID().GetGenreAsString()<<std::endl;
+
+	auto value = notification.GetValueID();
+	std::cout<<"GetValueLabel(): "<<OpenZWave::Manager::Get()->GetValueLabel(value)<<std::endl;
+	std::cout<<"GetValueUnits(): "<<OpenZWave::Manager::Get()->GetValueUnits(value)<<std::endl;
+	std::cout<<"GetValueMin(): "<<OpenZWave::Manager::Get()->GetValueMin(value)<<std::endl;
+	std::cout<<"GetValueMax(): "<<OpenZWave::Manager::Get()->GetValueMax(value)<<std::endl;
+	
+	std::string stringValue;
+	OpenZWave::Manager::Get()->GetValueAsString(value, &stringValue);
+	std::cout<<"GetValueAsString(): "<<stringValue<<std::endl;	
+	
 	return SensorData::Empty();
 }
